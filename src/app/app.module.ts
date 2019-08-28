faf14b4284fedaa3da16cc491db45cdf87f1e7a2
JossinAntony <jossin.antony@causalitybiomodels.com>
2019-08-28 15:37:37 +0530

    navbar integrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {Routes, RouterModule} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+ 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+const appRoutes:Routes=[
+  {path:'',component:NavbarComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,NavbarComponent]
 })
 export class AppModule { }

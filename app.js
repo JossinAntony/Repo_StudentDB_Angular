6c851bdb8d6e2eda91d5e5fa47a01821d5ea26d1
JossinAntony <jossin.antony@causalitybiomodels.com>
2019-08-29 10:52:48 +0530

    projectname changed in app.js

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/project-student'));
+app.use(express.static(__dirname + '/dist/projectStudent'));
 
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/project-student/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/projectStudent/index.html'));
   });
 
   app.listen(process.env.PORT || 3000);
